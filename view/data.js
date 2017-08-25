498d904e8eb663e674186f71983133d319574369
Gerald Joshua <gerald.joshua@g.austincc.edu>
2017-08-25 03:59:02 -0500

    Search Improvement

---
+++ view/data.js
@@ -0,0 +1,26 @@
+let allData = [
+  {
+    "universitas": "Akademi Komunitas Negeri Aceh Barat",
+  },
+  {
+    "universitas": "Institut Seni dan Budaya Indonesia Aceh",
+  },
+  {
+    "universitas": "Politeknik Negeri Lhokseumawe",
+  },
+  {
+    "universitas": "Universitas Malikussaleh",
+  },
+  {
+    "universitas": "Universitas Samudra",
+  },
+  {
+    "universitas": "Universitas Teuku Umar",
+  },
+  {
+    "universitas": "Universitas Sumatera Utara",
+  },
+  {
+    "universitas": "Universitas Negeri Medan",
+  }
+];
