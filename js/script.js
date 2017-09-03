51ca7e82c39b66d4f61a488b852e9270bfb8f9bb
tirto <tirtoraffles@gmail.com>
2017-09-03 14:42:58 +0700

    Course Code Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+	var courseCode_list = $(".courseCode-list");
     $('[data-toggle="popover"]').popover({trigger: "hover"});   
+	$(".btn-dropdown").mouseup(function() {
+		$(".courseCode-list").slideDown();
+    	$(".nama-matkul").focus();
+	});
+	$(".nama-matkul").blur(function() {
+	    $(".courseCode-list").slideUp();
+	});
 });
